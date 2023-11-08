2391bb8a3be8ffb21fc405bf0c963439cf7f666d
Samuel Dervishi <samueldervishi02@gmail.com>
2023-11-08 16:37:28 +0100

    added a simple check if any numbers are entered or not

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-// Funksioni:
 function age() {
     var d1 = document.getElementById("date").value;
     var m1 = document.getElementById("month").value;
     var y1 = document.getElementById("year").value;
+
+    if (!d1 || !m1 || !y1 || isNaN(d1) || isNaN(m1) || isNaN(y1)) {
+        alert("Please enter all the fields with valid numbers.");
+        return;
+    }
+
     var date = new Date();
     var d2 = date.getDate();
-    console.log(d2)
     var m2 = 1 + date.getMonth();
     var y2 = date.getFullYear();
     var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -18,13 +22,10 @@ function age() {
         m2 = m2 + 12;
         y2 = y2 - 1;
     }
-// Rezultati i moshes:
-   
+
     var d = d2 - d1;
     var m = m2 - m1;
     var y = y2 - y1;
     document.getElementById("age").innerHTML =
-        "You are " + y + " years old, " + m + " months and " + d + " days 😄!!!" + " <br> Congratulations and 100 more years 😇. ";
-
-
+        "You are " + y + " years old, " + m + " months and " + d + " days 😄!!!";
 }
